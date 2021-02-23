68d6049b6d96029ab56f71689330bd74273cbbe6
qunhuichen <qunhuichen@lexin.com>
2021-02-23 13:20:25 +0800

    初始化项目

---
+++ src/main/java/com/qunhuichen/source_code_learning_record/SourceCodeLearningRecordApplication.java
@@ -0,0 +1,13 @@
+package com.qunhuichen.source_code_learning_record;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SourceCodeLearningRecordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SourceCodeLearningRecordApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qunhuichen/source_code_learning_record/SourceCodeLearningRecordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qunhuichen.source_code_learning_record;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SourceCodeLearningRecordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
